b67cc9e77ae5ffe586056fe39f51e58ae167d4b8
Christian Tweed <christian.tweed@oncorps.io>
2024-07-23 01:25:25 -0400

    Added rendering clients from the bd

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,37 +1,20 @@
 import { injectLoad } from '@analogjs/router';
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
+
 import { load } from './index.server';
 
 @Component({
   imports: [MatTab, MatTabGroup],
   selector: 'app-home',
   standalone: true,
-  template: ` <mat-tab-group>
-    <mat-tab label="First"> Content 1 </mat-tab>
-    <mat-tab label="Second"> Content 2 </mat-tab>
-    <mat-tab label="Third"> Content 3 </mat-tab>
+  template: `<mat-tab-group>
+    @for(client of data().clients; track client.id) {
+    <mat-tab [label]="client.slug"> {{ client.name }}</mat-tab>
+    }
   </mat-tab-group>`,
-  styles: [
-    `
-      .logo {
-        will-change: filter;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .read-the-docs {
-        color: #888;
-      }
-    `,
-  ],
 })
 export default class HomeComponent {
   data = toSignal(injectLoad<typeof load>(), { requireSync: true });
-  count = signal(0);
-
-  increment() {
-    this.count.update((count) => count + 1);
-  }
 }
--- src/app/pages/index.server.ts
+++ src/app/pages/index.server.ts
@@ -1,14 +1,7 @@
 // src/app/pages/index.server.ts
-import { PageServerLoad } from '@analogjs/router';
 import prisma from '../../server/db';
 
-export const load = async ({
-  params, // params/queryParams from the request
-  req, // H3 Request
-  res, // H3 Response handler
-  fetch, // internal fetch for direct API calls,
-  event, // full request event
-}: PageServerLoad) => {
+export const load = async () => {
   const clients = await prisma.client.findMany();
 
   return {
